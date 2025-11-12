9dee8b273c92267707253ebeb1c5c3a4edff3d0f
Roman Lenshov <roman.lenshov@russianpost.ru>
2025-11-12 11:26:28 +0300

    add logs

--- src/workers/createCounterpartyWorker.ts
+++ src/workers/createCounterpartyWorker.ts
@@ -21,7 +21,7 @@ export const createCounterpartyWorker = {
         },
       }
     )
-
+    console.log({client})
     client.setSecurity(
       new soap.BasicAuthSecurity(
         process.env.USERNAME_1C as string,
